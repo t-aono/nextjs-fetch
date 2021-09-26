8626e08d1253bc4baa15ca772bc0ddda08bc3147
tkhs <spain3405@gmail.com>
2021-09-27 08:25:58 +0900

    first commit

---
+++ pages/posts/[id].js
@@ -0,0 +1,38 @@
+import Head from 'next/head'
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import utilStyles from '../../styles/utils.module.css'
+
+export default function Post({ postData }) {
+  return (
+    <Layout home={false}>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: postData.body }} />
+        <div className={utilStyles.idText}>
+          <small>ID { postData.id }</small>
+        </div>
+      </article>
+    </Layout>
+  )
+}
+
+export const getStaticPaths = async () => {
+  const paths = await getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
